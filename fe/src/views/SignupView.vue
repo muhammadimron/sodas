6bfe382c0b8a13a65e9a3ec27af6aa0603f65772
Muhammad Imron <muhammadimron1410@gmail.com>
2023-12-08 09:58:53 +0700

    [FE] Create static signup page

---
+++ fe/src/views/SignupView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+        <div class="main-left">
+            <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                <h1 class="mb-6 text-2xl">Sign Up</h1>
+                <p class="mb-6 text-gray-500">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod consequatur libero magni! Illo nulla quae animi explicabo iusto. Rem blanditiis earum, omnis quos sit harum. Veniam accusamus mollitia a?
+                </p>
+                <p class="font-bold">
+                    Already have an account? <a href="#" class="underline">Click here</a> to create one!
+                </p>
+            </div>
+        </div>
+        <div class="main-right">
+            <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                <form class="space-y-6">
+                    <div>
+                        <label>Name</label><br>
+                        <input type="text" placeholder="Your fullname" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <label>E-mail</label><br>
+                        <input type="email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <label>Password</label><br>
+                        <input type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <label>Repeat Password</label><br>
+                        <input type="password" placeholder="Repeat your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
+                    </div>
+                    <div>
+                        <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign Up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
