c127f5b1a7a77acc3f648a84851b474bd7812c05
Muhammad Imron <muhammadimron1410@gmail.com>
2023-12-11 10:00:08 +0700

    [BE] Create custom user model

--- be/sodasbe/settings.py
+++ be/sodasbe/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+AUTH_USER_MODEL = 'account.User'
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=180),
@@ -61,8 +63,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'djangorestframework_simplejwt',
+    'account',
+    'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders'
 ]
 
