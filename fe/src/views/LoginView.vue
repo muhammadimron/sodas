0b936fa6def11256ce2c6ec928221bd03430794f
Muhammad Imron <muhammadimron1410@gmail.com>
2023-12-15 09:37:57 +0700

    [FE] Reformat code and separate feed as a component

--- fe/src/views/LoginView.vue
+++ fe/src/views/LoginView.vue
@@ -1,91 +1,115 @@
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
-        <div class="main-left">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Login</h1>
-                <p class="mb-6 text-gray-500">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod consequatur libero magni! Illo nulla quae animi explicabo iusto. Rem blanditiis earum, omnis quos sit harum. Veniam accusamus mollitia a?
-                </p>
-                <p class="font-bold">
-                    Don't have an account? <RouterLink :to="{ 'name': 'signup' }" class="underline">Click here</RouterLink> to create one!
-                </p>
-            </div>
-        </div>
-        <div class="main-right">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <form class="space-y-6" @submit.prevent="handleSubmit()">
-                    <div>
-                        <label>E-mail</label><br>
-                        <input v-model="form.email" type="email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <div>
-                        <label>Password</label><br>
-                        <input v-model="form.password" type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <template v-if="errors.length > 0">
-                        <div class="bg-red-300 text-white rounded-lg p-6">
-                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
-                        </div>
-                    </template>
-                    <div>
-                        <button type="submit" class="py-4 px-6 bg-purple-600 text-white rounded-lg">Login</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+  <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+    <div class="main-left">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <h1 class="mb-6 text-2xl">Login</h1>
+        <p class="mb-6 text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod
+          consequatur libero magni! Illo nulla quae animi explicabo iusto. Rem
+          blanditiis earum, omnis quos sit harum. Veniam accusamus mollitia a?
+        </p>
+        <p class="font-bold">
+          Don't have an account?
+          <RouterLink :to="{ name: 'signup' }" class="underline"
+            >Click here</RouterLink
+          >
+          to create one!
+        </p>
+      </div>
     </div>
+    <div class="main-right">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <form class="space-y-6" @submit.prevent="handleSubmit()">
+          <div>
+            <label>E-mail</label><br />
+            <input
+              v-model="form.email"
+              type="email"
+              placeholder="Your e-mail address"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <label>Password</label><br />
+            <input
+              v-model="form.password"
+              type="password"
+              placeholder="Your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <template v-if="errors.length > 0">
+            <div class="bg-red-300 text-white rounded-lg p-6">
+              <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            </div>
+          </template>
+          <div>
+            <button
+              type="submit"
+              class="py-4 px-6 bg-purple-600 text-white rounded-lg"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import { useUserStore } from '@/stores/user';
-    export default {
-        setup(){
-            const userStore = useUserStore()
+import { useUserStore } from "@/stores/user";
+import axios from "axios";
+export default {
+  setup() {
+    const userStore = useUserStore();
 
-            return {
-                userStore
-            }
-        },
-        data() {
-            return {
-                form: {
-                    email: '',
-                    password: ''
-                },
-                errors: []
-            }
-        },
-        methods: {
-            async handleSubmit(){
-                this.errors = []
+    return {
+      userStore,
+    };
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+      errors: [],
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      this.errors = [];
 
-                if (this.form.email === '') {
-                    this.errors.push('Your e-mail is missing')
-                }
+      if (this.form.email === "") {
+        this.errors.push("Your e-mail is missing");
+      }
 
-                if (this.form.password === '') {
-                    this.errors.push('Your password is missing')
-                }
+      if (this.form.password === "") {
+        this.errors.push("Your password is missing");
+      }
 
-                if (this.errors.length === 0) {
-                    await axios.post('/api/login/', this.form)
-                        .then(response => {
-                            this.userStore.setToken(response.data)
+      if (this.errors.length === 0) {
+        await axios
+          .post("/api/login/", this.form)
+          .then((response) => {
+            this.userStore.setToken(response.data);
 
-                            axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.access
-                        })
-                        .catch(error => console.error(error))
+            axios.defaults.headers.common["Authorization"] =
+              "Bearer " + response.data.access;
+          })
+          .catch((error) => console.error(error));
 
-                    await axios.get('/api/me/')
-                        .then(response => {
-                            this.userStore.setUserInfo(response.data)
-                            
-                            this.$router.push('/feed')
-                        })
-                        .catch(error => console.error(error))
-                }
-            }
-        }
-    }
+        await axios
+          .get("/api/me/")
+          .then((response) => {
+            this.userStore.setUserInfo(response.data);
+
+            this.$router.push("/feed");
+          })
+          .catch((error) => console.error(error));
+      }
+    },
+  },
+};
 </script>
--- fe/src/views/SignupView.vue
+++ fe/src/views/SignupView.vue
@@ -1,114 +1,158 @@
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
-        <div class="main-left">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Sign Up</h1>
-                <p class="mb-6 text-gray-500">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod consequatur libero magni! Illo nulla quae animi explicabo iusto. Rem blanditiis earum, omnis quos sit harum. Veniam accusamus mollitia a?
-                </p>
-                <p class="font-bold">
-                    Already have an account? <RouterLink :to="{ 'name': 'login' }" class="underline">Click here</RouterLink> to login!
-                </p>
-            </div>
-        </div>
-        <div class="main-right">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <form class="space-y-6" @submit.prevent="submitForm()">
-                    <div>
-                        <label>Name</label><br>
-                        <input type="text" v-model="form.name" placeholder="Your fullname" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <div>
-                        <label>E-mail</label><br>
-                        <input type="email" v-model="form.email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <div>
-                        <label>Password</label><br>
-                        <input type="password" v-model="form.password1" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <div>
-                        <label>Repeat Password</label><br>
-                        <input type="password" v-model="form.password2" placeholder="Repeat your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-                    <template v-if="errors.length > 0">
-                        <div class="bg-red-300 text-white rounded-lg p-6">
-                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
-                        </div>
-                    </template>
-                    <div>
-                        <button type="submit" class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign Up</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+  <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+    <div class="main-left">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <h1 class="mb-6 text-2xl">Sign Up</h1>
+        <p class="mb-6 text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod
+          consequatur libero magni! Illo nulla quae animi explicabo iusto. Rem
+          blanditiis earum, omnis quos sit harum. Veniam accusamus mollitia a?
+        </p>
+        <p class="font-bold">
+          Already have an account?
+          <RouterLink :to="{ name: 'login' }" class="underline"
+            >Click here</RouterLink
+          >
+          to login!
+        </p>
+      </div>
     </div>
+    <div class="main-right">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <form class="space-y-6" @submit.prevent="submitForm()">
+          <div>
+            <label>Name</label><br />
+            <input
+              type="text"
+              v-model="form.name"
+              placeholder="Your fullname"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <label>E-mail</label><br />
+            <input
+              type="email"
+              v-model="form.email"
+              placeholder="Your e-mail address"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <label>Password</label><br />
+            <input
+              type="password"
+              v-model="form.password1"
+              placeholder="Your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <label>Repeat Password</label><br />
+            <input
+              type="password"
+              v-model="form.password2"
+              placeholder="Repeat your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <template v-if="errors.length > 0">
+            <div class="bg-red-300 text-white rounded-lg p-6">
+              <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            </div>
+          </template>
+          <div>
+            <button
+              type="submit"
+              class="py-4 px-6 bg-purple-600 text-white rounded-lg"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import { useToastStore } from '@/stores/toast';
-    import axios from 'axios'
+import { useToastStore } from "@/stores/toast";
+import axios from "axios";
 
-    export default {
-        setup() {
-            const toastStore = useToastStore()
+export default {
+  setup() {
+    const toastStore = useToastStore();
 
-            return {
-                toastStore
+    return {
+      toastStore,
+    };
+  },
+
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        password1: "",
+        password2: "",
+      },
+      errors: [],
+    };
+  },
+
+  methods: {
+    submitForm() {
+      this.errors = [];
+
+      if (this.form.email === "") {
+        this.errors.push("Your e-mail is missing");
+      }
+
+      if (this.form.name === "") {
+        this.errors.push("Your name is missing");
+      }
+
+      if (this.form.password1 === "") {
+        this.errors.push("Your password is missing");
+      }
+
+      if (this.form.password1 !== this.form.password2) {
+        this.errors.push("The password does not match");
+      }
+
+      if (this.errors.length === 0) {
+        axios
+          .post("/api/signup/", this.form)
+          .then((response) => {
+            if (response.status === 201) {
+              this.toastStore.showToast(
+                5000,
+                "The user is registered. Please log in",
+                "bg-emerald-500"
+              );
+
+              this.form.email = "";
+              this.form.name = "";
+              this.form.password1 = "";
+              this.form.password2 = "";
+            } else {
+              this.toastStore.showToast(
+                5000,
+                "Something went wrong. Please try again",
+                "bg-red-300"
+              );
             }
-        },
-
-        data() {
-            return {
-                form: {
-                    email: '',
-                    name: '',
-                    password1: '',
-                    password2: ''
-                },
-                errors: []
-            }
-        },
-
-        methods: {
-            submitForm() {
-                this.errors = []
-
-                if (this.form.email === '') {
-                    this.errors.push('Your e-mail is missing')
-                }
-
-                if (this.form.name === '') {
-                    this.errors.push('Your name is missing')
-                }
-
-                if (this.form.password1 === '') {
-                    this.errors.push('Your password is missing')
-                }
-
-                if (this.form.password1 !== this.form.password2) {
-                    this.errors.push('The password does not match')
-                }
-
-                if (this.errors.length === 0) {
-                    axios.post('/api/signup/', this.form)
-                        .then((response) => {
-                            if (response.status === 201) {
-                                this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
-                                
-                                this.form.email = ''
-                                this.form.name = ''
-                                this.form.password1 = ''
-                                this.form.password2 = ''
-                            } else {
-                                this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-red-300')
-                            }
-                        })
-                        .catch((error) => {
-                            console.error('error', error)
-                            this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-red-300')
-                        })
-                }
-            }
-        }
-    }
+          })
+          .catch((error) => {
+            console.error("error", error);
+            this.toastStore.showToast(
+              5000,
+              "Something went wrong. Please try again",
+              "bg-red-300"
+            );
+          });
+      }
+    },
+  },
+};
 </script>
